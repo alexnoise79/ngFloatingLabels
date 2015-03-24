9aee31dc5227dfd391cfd409419259bbff9b20b4
Alex Vitari <alexnoise79@gmail.com>
2015-03-24 16:19:53 +0100

    add async validation example

--- example/app.js
+++ example/app.js
@@ -16,19 +16,62 @@ var messages = {
     pattern: "don\'t match the pattern",
     "email": "mail address not valid",
     "number": "insert only numbers",
-    "custom" : "custom not valid type \"@value@\""
+    "custom": "custom not valid type \"@value@\"",
+    "async": "async not valid type \"@value@\""
 };
 
 
 var app = angular.module('app', ['ngFloatingLabels']);
 
-app.directive('customValidator', function() {
+app.directive('customValidator', function () {
     return {
-        require : 'ngModel',
-        link : function(scope, element, attrs, ngModel) {
-            ngModel.$validators.custom = function(value) {
-                return value === "ciao";
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+            ngModel.$validators.custom = function (value) {
+                return value === "foo";
             };
         }
     };
 });
+
+app.service('$fakeValidationService', ['$q', function ($q) {
+    return {
+        "get": function (value) {
+            var deferred = $q.defer();
+
+            setTimeout(function () {
+                if (value === "bar") {
+                    deferred.resolve({valid: true});
+                } else {
+                    deferred.reject({valid: false});
+                }
+            }, 3000);
+
+            return deferred.promise;
+        }
+    }
+}])
+
+app.directive('asyncValidator', ['$fakeValidationService', '$q', function ($fakeValidationService, $q) {
+    return {
+        require: 'ngModel',
+        link: function ($scope, element, attrs, ngModel) {
+            ngModel.$asyncValidators.async = function (modelValue, viewValue) {
+                var value = modelValue || viewValue;
+                if(value.length){
+                    element.before('<i class="icon-spin icon-refresh"></i>').parent().addClass('spinner');
+
+                    return $fakeValidationService.get(value).then(function(response) {
+                        element.parent().removeClass('spinner').find('i').remove();
+                        return true;
+                    }, function(response) {
+                        element.parent().removeClass('spinner').find('i').remove();
+                        if (!response.valid) {
+                            return $q.reject();
+                        }
+                    });
+                }
+            };
+        }
+    }
+}])
